01b072cea0621bea175bcdd3013cfdfab3dfd0a4
sandra7968 <sandrapylee@gmail.com>
2023-11-25 00:29:06 +0530

    project

--- Controllers/projectController.js
+++ Controllers/projectController.js
@@ -1,9 +1,23 @@
 const projects = require('../Models/projectSchema')
 
 // .add project
-exports.addProjects = (req,res)=>{
+exports.addProjects =  async (req,res)=>{
     console.log("Inside add project function");
     const userId = req.payload
-    console.log(userId);
-    res.status(200).json("addProjects request received")
-}
+    const projectImage = req.file.filename
+    const {title,languages,overview,github,website} = req.body
+    // console.log(title,languages,overview,github,website,projectImage,userId);
+    try{
+        const existingProject = await projects.findOne({github})
+        if(existingProject){
+            res.status(406).json('Project already exist! Upload another')
+        }else{
+            const newProject = new projects({
+                title,languages,overview,github,website,projectImage,userId
+            })
+            await newProject.save()
+            res.status(200).json(newProject)
+        }
+    }catch(err){
+        res.status(401).json(`Request failed, Error:${err}`)
+    }}
--- Routes/router.js
+++ Routes/router.js
@@ -3,11 +3,12 @@ const router = new express.Router()
 const userController = require('../Controllers/userController')
 const projectController = require('../Controllers/projectController')
 const jwtMiddleware = require('../Middlewares/jwtMiddleware')
+const multerConfig = require('../Middlewares/multerMiddleware')
 // register API
 router.post('/user/register',userController.register)
 // login
 router.post('/user/login',userController.login)
 // add-project
-router.post('/project/add',jwtMiddleware,projectController.addProjects)
+router.post('/project/add',jwtMiddleware,multerConfig.single('projectImage'),projectController.addProjects)
 // export router
 module.exports = router
